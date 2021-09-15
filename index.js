06ab4260e5752a4692ba4d651028a25cb557e3bc
7AMO_GAMING <82784417+hamogaming@users.noreply.github.com>
2021-09-15 09:58:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ client.on('message', message => {
 
 if (!message.guild) return; 
 
-if (message.content === 'p+wara') { 
+if (message.content === '*wara vc') { 
  if (!message.member.hasPermission("ADMINISTRATOR"))  return;
 if (message.member.voiceChannel) { 
 
@@ -36,9 +36,9 @@ if(message.channel.type === 'dm') {
 
 if (message.content.startsWith("https://discord.gg/")) {
     
-  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/EXCuvNd`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
+  message.author.send(` دانرا تۆش ڕیکلامەکە بکە https://discord.gg/j2FVjuE3VN`) // لینکی سێرڤەری خۆت لیرە دابنی بە هەتا هەتای بێت
     
-  client.channels.get("755636700482633791").send(
+  client.channels.get("818440403266043905").send(
 `> <:ARBELE_UPUP:791411050238836766> By: <@${message.author.id}> ✅ 
 ${message.content}`)
 }
@@ -51,10 +51,10 @@ ${message.content}`)
 client.on('ready', () => {
 console.log(`ON ${client.guilds.size} Servers`);
 console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`ڕیکلامەکە بنێرە بۆمن دایدەنێم☑️`) 
+client.user.setGame(`Partner Bot Of 5pro ڕیکلامەکە بنێرە بۆمن دایدەنێم☑️`) 
 });
 
 
 
 
-client.login("");//تۆکین لێرە دانێ
+client.login("ODg2NjQyOTMyMTg5Mzg0NzY1.YT4klA.jiY2IIPVsekYP5cfOX0kMUS2e7s");//تۆکین لێرە دانێ
